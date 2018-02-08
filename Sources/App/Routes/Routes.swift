ce786c803a8463b7ce7261b8dc9c3ff065aa5b9b
jcabral32 <jeancabral@Jeans-MacBook-Pro-2.local>
2018-02-08 18:40:39 -0500

    first commit

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,25 +1,45 @@
 import Vapor
+import QuartzCore
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
+        
+        
+        func executionTimeInterval(block: () -> ()) -> CFTimeInterval {
+            let start = CACurrentMediaTime()
+            block();
+            let end = CACurrentMediaTime()
+            return end - start
+        }
+
+        
+        
+        
+        post("inList"){ request in
+            var errorMessage = JSON()
+            guard let array = request.json?["inList"]?.array else {
+                try errorMessage.set("message", request.description)
+                return errorMessage
+                //fatalError("Required parameter was not found. The paramter was\(request.data)")
+            }
+            
+            var tempArray = [Int]()
+            for numbers in array.indices {
+                tempArray.append(array[numbers].int!)
+            }
+            let timeInterval = executionTimeInterval {
+                tempArray.sort()
+            }
+            var milliseconds = (timeInterval.doubleValue / 1000)
+            milliseconds.round(FloatingPointRoundingRule.up)
+            
             var json = JSON()
-            try json.set("hello", "world")
+            try json.set("outList", tempArray)
+            try json.set("algorithim", "insertionSort")
+            try json.set("timeMS:", milliseconds)
+            
+            
             return json
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
     }
 }
